7eb02ba1d9fb5bd5b1a8f8183101dea1ad1207df
rinooooo <rino.0566@gmail.com>
2022-08-30 00:12:05 +0900

    postコントローラーでhtmlの読み込み

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class PostController {
     @GetMapping("/")
-    @ResponseBody
     public String showHello(){
-        return "<h1>Hello World!</h1>";
+        return "hello";
     }
 }
 
